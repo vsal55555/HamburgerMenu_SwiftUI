594eb60790e37bda9de1e01c31296832c22ddc71
vsal55555 <vsal55555@gmail.com>
2020-08-21 12:03:48 +0545

    Initial Commit

---
+++ HamburgerMenu_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HamburgerMenu_SwiftUI
+//
+//  Created by BSAL-MAC on 8/21/20.
+//  Copyright © 2020 BSAL-MAC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
